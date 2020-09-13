c4a81ed98e756c5ca6f3fb8f6fcdb1bc3849d35f
lucasebeninca <lebbeninca@gmail.com>
2020-09-13 13:33:25 -0300

    create, insert, upadate e delete + trabalhando com filtros WHERE

--- Postgre/query.sql
+++ Postgre/query.sql
@@ -12,6 +12,41 @@ CREATE TABLE aluno(
 	matriculado_em timestamp
 );
 
+SELECT * FROM aluno;
 
 INSERT INTO aluno (nome, cpf, observacao, idade, dinheiro, altura, ativo, data_nascimento, hora_aula, matriculado_em) 
 	VALUES ('Lucas', '12345678901', 'lorem alguma coisa', 24, 100.0, 1.76, TRUE,'1996-01-03', '00:00:00', '20-09-2020 12:32:45' );
+	
+SELECT * FROM aluno WHERE id = 1;
+
+UPDATE aluno SET
+	nome = 'beninca',
+	cpf = '01987654321',
+	observacao = 'texto update',
+	idade = 25,
+	dinheiro = 50.0,
+	altura = 1.76,
+	ativo = FALSE,
+	data_nascimento = '1996-01-03',
+	hora_aula = '00:00:01',
+	matriculado_em  = '2020-01-02 15:00:00'
+WHERE id = 1;
+	
+	DELETE FROM aluno WHERE nome = 'beninca';
+	
+SELECT nome AS "nome do aluno", 
+	idade, 
+	matriculado_em AS quando_se_matriculou
+	FROM aluno;
+	
+INSERT INTO aluno (nome) values('vinicius dias');
+INSERT INTO aluno (nome) values('rafael jose');
+INSERT INTO aluno (nome) values('tiago josue');
+INSERT INTO aluno (nome) values('vinicius');
+
+SELECT * FROM aluno
+	WHERE nome = 
+
+	
+	
+	
